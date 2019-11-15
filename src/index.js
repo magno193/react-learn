7f690ab7ce092d88e1db087ee70704e61ceed4ad
Alexandre Ferreira <alexmlf93@gmail.com>
2019-11-15 00:35:02 -0300

    Exemplo 11 - Extraindo Componentes pt1

--- src/index.js
+++ src/index.js
@@ -2,16 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import App from './App';
 
-function Welcome(props) {
-    return <h1>Hello, {props.name}</h1>
-}
+function Comment(props) {
+    return(
+        <div className="Comment">
 
-function App(){
-    return (
-        <div>
-            <Welcome name="Sara"/>
-            <Welcome name="Cahal"/>
-            <Welcome name="Edite"/>
+            <div className="UserInfo">
+                <img
+                    src={props.author.avatarUrl}
+                    alt={props.author.name}
+                />
+                <div className="UserInfo-name">
+                    {props.author.name}
+                </div>
+            </div>
+
+            <div className="Comment-text">
+                {props.text}
+            </div>
+            <div className="Comment-date">
+                {formatDate(props.date)}
+            </div>
+
         </div>
     );
 }
