48005a6edef3655a8b19edeae47fa687d3f28c24
Alexandre Ferreira <alexmlf93@gmail.com>
2019-11-14 22:37:06 -0300

    Exemplo 8 - Componente feito com classe (n recomendavel)

--- src/index.js
+++ src/index.js
@@ -2,22 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-// Elementos react são constantes. A unica forma de atualizar a UI
-// é criando um novo elemento e passando para ReactDOM.render
-
-function tick() {
-    const element = (
-        <div>
-            <h1>Hello world!</h1>
-            <h2>It is {new Date().toLocaleTimeString()}.</h2>
-        </div>
-    );
-
-    ReactDOM.render( 
-        element,
-        document.getElementById('root')
-    );
+class Welcome extends React.Component {
+    render() {
+    return <h1>Hello, stranger</h1>
+    }
 }
 
-// Chama tick a cada 1 segundo
-setInterval(tick, 1000);
+ReactDOM.render(
+    <Welcome />,
+    document.getElementById('root')
+);
