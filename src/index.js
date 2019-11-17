d4992ca749e8c5b77872329fa9f9551beccb5626
Alexandre Ferreira <alexmlf93@gmail.com>
2019-11-17 00:50:23 -0300

    Exemplo 13 - Eventos Toggle

--- src/index.js
+++ src/index.js
@@ -2,56 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import App from './App';
 
-class Clock extends React.Component{
-  constructor(props){
+class Toggle extends React.Component {
+  constructor(props) {
     super(props);
-    this.state = {date: new Date()}
+    this.state = { isToggleOn: true };
+
+  // bind é utilizado para que this funcione dentro da callback
+  this.handleClick = this.handleClick.bind(this);
   }
 
-  // Ocorre sempre que DOM de Clock é renderizado pela primeira vez
-  componentDidMount(){
-    this.timerID = setInterval(
-      () => this.tick(),
-      1000
-    );
+  handleClick() {
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn
+    }));
   }
 
-  // Ocorre sempre quando a DOM de Clock é removido
-  componentWillUnmount(){
-    clearInterval(this.timerID);
+  render() {
+      return (
+        <button onClick={this.handleClick}>
+          {this.state.isToggleOn ? 'ON' : 'OFF'}
+        </button>
+      );
   }
-
-  tick(){
-    this.setState({
-      date: new Date()
-    });
-  }
-
-  render(){
-    return (
-      <div>
-        <h1>Hello, Stranger.</h1>
-        <FormattedDate date={this.state.date} />
-      </div>
-    );
-  }
-}
-
-function FormattedDate(props) {
-  return <h2>It is {props.date.toLocaleTimeString()}</h2>
-}
-
-function App() {
-  return(
-    <div>
-      <Clock />
-      <Clock />
-      <Clock />
-    </div>
-  )
 }
 
 ReactDOM.render(
-  <App />,
+  <Toggle />,
   document.getElementById('root')
 );
