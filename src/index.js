a7ae302c63877b8e1e2cb7065ad68122ec01316d
Alexandre Ferreira <alexmlf93@gmail.com>
2019-11-19 03:18:03 -0300

    Exemplo 14 - Condicional pt2

--- src/index.js
+++ src/index.js
@@ -2,27 +2,75 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import App from './App';
 
+function UserGreeting() {
+  return <h1>Welcome back!</h1>
+}
+
+function GuestGreeting() {
+  return <h1>Please, log in.</h1>
+}
+
 function Greeting(props) {
   const isLoggedIn = props.isLoggedIn;
 
   if (isLoggedIn) {
     return <UserGreeting />
-  } 
-
+  }
   return <GuestGreeting />
 }
 
-function UserGreeting(props) {
-  return <h1>Welcome back!</h1>
+class LoginControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isLoggedIn: false}
+  }
+
+  handleLoginClick() {
+    this.setState({isLoggedIn: true})
+  }
+
+  handleLogoutClick() {
+    this.setState({isLoggedIn: false})
+  }
+
+  render() {
+    const isLoggedIn = this.state.isLoggedIn;
+    let button;
+
+    if (isLoggedIn) {
+      button = <LogoutButton onClick={this.handleLogoutClick} />;
+    } else {
+      button = <LoginButton onClick={this.handleLoginClick} />
+    }
+
+    return(
+      <div>
+        <Greeting isLoggedIn={isLoggedIn} />
+        {button}
+      </div>
+    )
+  }
 }
 
-function GuestGreeting(props) {
-  return <h1>Please, sign up.</h1>
+function LoginButton(props) {
+  return(
+    <button onClick={props.onClick}>
+      Login
+    </button>
+  );
+}
+
+function LogoutButton(props) {
+  return(
+    <button onClick={props.onClick}>
+      Logout
+    </button>
+  );
 }
 
 // renderiza um valor diferente dependendo do valor
 // da prop isLoggedIn
 ReactDOM.render(
-  <Greeting isLoggedIn={false} />,
+  <LoginControl />,
   document.getElementById('root')
 );
