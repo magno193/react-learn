5e100d13807b766cf6f42447ce1bcdd3b4a3cc7d
magno193 <34192221+magno193@users.noreply.github.com>
2019-11-20 01:25:50 -0200

    Exemplo 15 - Lista e Chaves pt3

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import App from './App';
 
+function ListItem(props) {
+  return <li>{props.item}</li>
+}
+
 function NumbersList(props) {
   const numbers = props.numbers;
   const listItens = numbers.map((number) =>
-    <li key={number.toString()}>
-      {number}
-    </li>
+    <ListItem key={number.toString()} item={number}/>
   );
-  return <ul>{listItens}</ul>
+  return (
+    <ul>
+      {listItens}
+    </ul>
+  );
 }
 
 const numbers = [1, 5, 7, 23, 34]
