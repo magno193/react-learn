eea00e2d2e710cbc28fdb18f93fa30be23568be5
Alexandre Ferreira <alexmlf93@gmail.com>
2019-11-21 01:33:00 -0300

    Exemplo 16 - Forms pt2

--- src/index.js
+++ src/index.js
@@ -2,34 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import App from './App';
 
-class NameForm extends React.Component{
-  constructor(props) {
+class EssayForm extends React.Component {
+  constructor(props){
     super(props);
-    this.state = {value: ''};
+    this.state= {
+      value: 'Escreva uma dissertação sobre seu elemento DOM favorito.'
+    }
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    this.setState({value: event.target.value.toUpperCase()})
+    this.setState({value: event.target.value});
   }
 
   handleSubmit(event) {
-    alert("Um nome foi enviado: " + this.state.value);
+    alert('Uma dissertação foi enviada: ' + this.state.value)
     event.preventDefault();
   }
 
-  render() {
+  render(){
     return(
       <form onSubmit={this.handleSubmit}>
         <label>
-          Nome:
-          <input
-            type="text" 
-            value={this.state.value}
-            onChange={this.handleChange}
-          />
+          Dissertação:
+          <textarea value={this.state.value} onChange={this.handleChange} />
         </label>
         <input type="submit" value="Enviar" />
       </form>
@@ -38,6 +36,6 @@ class NameForm extends React.Component{
 }
 
 ReactDOM.render(
-  <NameForm />,
+  <EssayForm />,
   document.getElementById('root')
 );
