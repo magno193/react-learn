553f348050aafff4c96029537be12ed54a540edf
Alexandre Ferreira <alexmlf93@gmail.com>
2019-11-25 16:59:58 -0300

    Exemplo 19 - React pt2

--- src/index.js
+++ src/index.js
@@ -2,43 +2,125 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import App from './App';
 
+const PRODUCTS = [
+  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football' },
+  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball' },
+  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball' },
+  {category: 'Eletronics', price: '$99.99', stocked: true, name: 'iPod Touch' },
+  {category: 'Eletronics', price: '$399.99', stocked: false, name: 'iPhone 5' },
+  {category: 'Eletronics', price: '$199.99', stocked: true, name: 'Nexus 7' }
+];
+
 /*@function 
 * Contém a totalidade do exemplo
 */
-function FilterableProductTable(props) {
-  return(<p></p>);
+class FilterableProductTable extends React.Component {
+  render(){
+    return(
+      <div>
+        <SearchBar />
+        <ProductTable products={this.props.products} />
+      </div>
+    );
+  }
 }
 
 /*@function 
 * Recebe todo o input do usuário
 */
-function SearchBar(props) {
-  
+class SearchBar extends React.Component {
+  render(){
+    return(
+      <form>
+        <input type="text"  placeholder="Search..." />
+        <p>
+          <input type="checkbox" />
+          {' '}
+          Only show products in stock
+        </p>
+      </form>
+    );
+  }
 }
 
 /*@function 
 * Exibe e filtra a coleção de dados baseado
 * no input do usuário
 */
-function ProductTable(props) {
-  
+class ProductTable extends React.Component {
+  render(){
+    const rows = [];
+    let lastCategory = null;
+
+    this.props.products.forEach((product) => {
+      if (product.category !== lastCategory) {
+        rows.push(
+          <ProductCategoryRow
+            category={product.category}
+            key={product.category} />
+        );
+      }
+      rows.push(
+        <ProductRow
+          product={product}
+          key={product.name} />
+      );
+      lastCategory = product.category;
+    });
+
+    return(
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbo>{rows}</tbo>
+      </table>
+    );
+  }
 }
 
-/*@function 
+/*@class 
 * Exibe um cabeçalho para cada categoria
 */
-function ProductCategoryRow(props) {
-  
+class ProductCategoryRow extends React.Component {
+  render() {
+    const category = this.props.category;
+
+    return(
+      <tr>
+        <th colSpan="2">
+          {category}
+        </th>
+      </tr>
+    );
+  }
 }
 
-/*@function 
+/*@class 
 * Exibe uma linha para cada produto
 */
-function ProductRow(props) {
-  
+class ProductRow extends React.Component {
+  render() {
+    const product = this.props.product;
+    const name = product.stocked ? 
+      product.name :
+      <span style={{color: "red"}}>
+        {product.name}
+      </span>
+
+  return(
+    <tr>
+      <td>{name}</td>
+      <td>{product.price}</td>
+    </tr>
+  );
+  }
 }
 
 ReactDOM.render(
-  <FilterableProductTable />,
+  <FilterableProductTable products={PRODUCTS}/>,
   document.getElementById('root')
 );
