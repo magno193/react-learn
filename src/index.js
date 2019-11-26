cfa410b9b14edc82fb16c8f9f42ad90d4d1c6aac
Alexandre Ferreira <alexmlf93@gmail.com>
2019-11-26 14:00:17 -0300

    Exemplo 19 - React pt3

--- src/index.js
+++ src/index.js
@@ -15,11 +15,26 @@ const PRODUCTS = [
 * Contém a totalidade do exemplo
 */
 class FilterableProductTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterText: '',
+      inStockOnly: false
+    };
+  }
+
   render(){
     return(
       <div>
-        <SearchBar />
-        <ProductTable products={this.props.products} />
+        <SearchBar
+          filterText={this.state.filterText}
+          inStockOnly={this.state.inStockOnly}
+        />
+        <ProductTable
+          products={this.props.products}
+          filterText={this.state.filterText}
+          inStockOnly={this.state.inStockOnly}
+        />
       </div>
     );
   }
@@ -30,11 +45,21 @@ class FilterableProductTable extends React.Component {
 */
 class SearchBar extends React.Component {
   render(){
+    const filterText = this.props.filterText;
+    const inStockOnly = this.props.inStockOnly;
+
     return(
       <form>
-        <input type="text"  placeholder="Search..." />
+        <input
+          type="text"
+          placeholder="Search..."
+          value={filterText}
+        />
         <p>
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            checked={inStockOnly}
+          />
           {' '}
           Only show products in stock
         </p>
@@ -49,17 +74,28 @@ class SearchBar extends React.Component {
 */
 class ProductTable extends React.Component {
   render(){
+    const filterText = this.props.filterText;
+    const inStockOnly = this.props.inStockOnly;
+
     const rows = [];
     let lastCategory = null;
 
     this.props.products.forEach((product) => {
+      if (product.name.indexOf(filterText) === -1) {
+        return;
+      }
+      if (inStockOnly && !product.stocked) {
+        return;
+      }
       if (product.category !== lastCategory) {
         rows.push(
           <ProductCategoryRow
             category={product.category}
-            key={product.category} />
+            key={product.category}
+          />
         );
       }
+      
       rows.push(
         <ProductRow
           product={product}
